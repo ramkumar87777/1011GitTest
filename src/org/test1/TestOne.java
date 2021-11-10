ffa08dd0a888e5c109f867cec53f8d80544f50ca
ramkumar87777 <ramsuryasankararudv@gmail.com>
2021-11-10 19:11:45 +0530

    gittest

---
+++ src/org/test1/TestOne.java
@@ -0,0 +1,13 @@
+package org.test1;
+
+public class TestOne {
+private void empName() {
+	System.out.println("Ramkumar");
+}
+public static void main(String[] args) {
+	TestOne t=new TestOne();
+	t.empName();
+}
+
+
+}
